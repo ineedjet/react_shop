4a2e1b7ea47a0ebb4af4d9b2f38e351b556b7082
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-08-17 00:03:59 +0300

    add routes helper

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import {products} from './src/constants/Products';
 import Catalog from "./src/components/Catalog";
 import CartContext from './src/components/CartContext';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { mainPath, cartPath, contactsPath, productPath } from './src/helpers/routes';
+
+const Product = ({ match: { params } }) => {
+  return (<div>Product id is id { params.id }</div>)
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {cartList: []};
-
     this.addToCart = this.addToCart.bind(this);
   }
 
@@ -27,7 +31,14 @@ class App extends Component {
         <div>
           <Router>
             <Switch>
-              <Route exact strict path='/app' render={()=> <Catalog products={products}/> } />
+              <Route exact strict path={mainPath()} render={ () => <Catalog products={products}/> } />
+              <Route exact strict
+                     path={productPath()}
+                     component={Product}
+              />
+              <Route exact strict path={cartPath()} render={ () => <div>Cart</div> } />
+              <Route exact strict path={contactsPath()} render={ () => <div>Contacts</div> } />
+              <Route exact strict path='' render={ () => <div>404</div> } />
             </Switch>
           </Router>
         </div>
