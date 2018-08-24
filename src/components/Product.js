feda25569d5ecdf919c65a8af8421180ef7ec858
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-08-25 01:51:22 +0300

    add product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,18 @@
 import {Component} from "react";
 import React from "react";
+import {products} from '../constants/Products';
+import ProductCard from './ProductCard';
+import Cart from './Cart';
 
 class Product extends Component {
   render() {
+    const product = products.find( item => item.id === parseInt(this.props.id, 10) );
+
     return (
       <div>
-       <h1>Product { this.props.id }</h1>
+        <h1>Product { product.id }</h1>
+        <Cart/>
+        <ProductCard product={product} />
       </div>
     )
   }
