1849e3d32bd9b4db58e8d2e60ecbc75c5e09dd28
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-09-19 17:54:08 +0300

    add store, actions redusers

---
+++ src/actions/catalog.js
@@ -0,0 +1,3 @@
+export const fetchProductsRequest = () => ({ type: 'FETCH_PRODUCTS_REQUEST' });
+export const fetchProductsSuccess = () => ({ type: 'FETCH_PRODUCTS_SUCCESS' });
+export const fetchProductsfaulure = () => ({ type: 'FETCH_PRODUCTS_FAILURE' });
---
+++ src/redusers/catalog.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+  entries: []
+};
+
+const catalog = (state = INITIAL_STATE, action) =>{
+  switch(action.type){
+    default:
+      return state;
+  }
+};
