b7b5913113b4f272e5c7ce4ceb2def870e39217a
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-09-20 03:09:59 +0300

    little clean up

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -5,8 +5,6 @@ const INITIAL_STATE = {
 const cart = (state = INITIAL_STATE, action) =>{
   switch(action.type){
     case 'ADD_TO_CART':
-      console.log('s', state);
-      console.log('i', action.item);
       return { entries: [...state.entries, action.item] };
 
     default:
--- src/store/index.js
+++ src/store/index.js
@@ -2,18 +2,9 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
-const logger = store => next => action => {
-  console.log('Middleware logger: [store] -> ', store.getState());
-  setTimeout(function() {
-    console.log('Middleware logger 2s: [store]-> ', store.getState());
-  }, 2000);
-  next(action);
-};
-
-
 const store = createStore(
   combineReducers(reducers),
-  applyMiddleware(thunk, logger)
+  applyMiddleware(thunk)
 );
 
 export default store;
