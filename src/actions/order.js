0be020f19a1611a333b60563b4e008e8bffd71b6
Yuriy Kirillov <yurkakirillov@gmail.com>
2018-09-21 16:32:44 +0300

    send order json

--- src/actions/order.js
+++ src/actions/order.js
@@ -1,3 +1,19 @@
-export const submitOrder = () => ({
-  type: 'SUBMIT_ORDER',
+import { API_CALL } from '../middleware/API'
+import {accessToken, apiUrl, environments, spaces} from "../constants/access";
+
+
+export const submitOrder = (values) => ({
+  [API_CALL]: {
+    root: apiUrl,
+    endpoint: `/spaces/${spaces}/environments/${environments}/orders`,
+    method: 'POST',
+    accessToken: accessToken,
+    query: { 'content_type': 'product' },
+    payload: values,
+    types: [
+      'SUBMIT_ORDER_REQUEST',
+      'SUBMIT_ORDER_SUCCESS',
+      'SUBMIT_ORDER_FAILURE'
+    ]
+  }
 });
